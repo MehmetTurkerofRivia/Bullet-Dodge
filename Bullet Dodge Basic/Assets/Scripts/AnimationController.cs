9830ffe7f11cb0159a986cab8ed5bb4aa2694a03
Mehmet Türker <mertmemo69@gmail.com>
2024-05-24 12:49:21 +0300

    a

--- Bullet Dodge Basic/Assets/Scripts/AnimationController.cs
+++ Bullet Dodge Basic/Assets/Scripts/AnimationController.cs
@@ -5,20 +5,21 @@ using UnityEngine;
 public class AnimationController : MonoBehaviour
 {
     private Animator animator;
+    private bool isHit = false;
     void Start()
     {
         animator = GetComponent<Animator>();    
     }
     void Update()
     {
-        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        if((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)) && isHit==false)
         {
             animator.SetInteger("AnimSelect", 1);
         }
         else
             animator.SetInteger("AnimSelect", 0);
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonUp(0) && isHit == false)
         {
             StartCoroutine(HitAnim());
         }       
@@ -26,11 +27,11 @@ public class AnimationController : MonoBehaviour
 
     IEnumerator HitAnim()
     {
+        yield return new WaitForSeconds(1f);
         animator.SetInteger("AnimSelect", 2);
+        isHit = true;
         yield return new WaitForSeconds(3f);
         animator.SetInteger("AnimSelect", 0);
-
-
-
+        isHit= false;
     }
 }

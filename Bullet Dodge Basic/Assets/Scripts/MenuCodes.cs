4dea4381f525b78a3599d6f100346423443033ac
Mehmet Türker <mertmemo69@gmail.com>
2024-07-22 00:52:12 +0300

    a

--- Bullet Dodge Basic/Assets/Scripts/MenuCodes.cs
+++ Bullet Dodge Basic/Assets/Scripts/MenuCodes.cs
@@ -5,9 +5,5 @@ using UnityEngine;
 
 public class MenuCodes : MonoBehaviour
 {
-    [SerializeField] GameObject text;      
-    void Update()
-    {
-        text.transform.position = Vector3.MoveTowards(text.transform.position, new Vector3());
-    }
+    
 }

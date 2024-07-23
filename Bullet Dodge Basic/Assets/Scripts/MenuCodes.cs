32f7edf35d9552e440272350503f649fffac553c
Mehmet Türker <mertmemo69@gmail.com>
2024-07-23 15:05:24 +0300

    a

--- Bullet Dodge Basic/Assets/Scripts/MenuCodes.cs
+++ Bullet Dodge Basic/Assets/Scripts/MenuCodes.cs
@@ -2,8 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuCodes : MonoBehaviour
 {
-    
+    public void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
